50e9201aed1650066c331cc36213bdb8e7aa3bdc
mnemonicj <j.orlandomurillo@gmail.com>
2016-08-30 17:46:26 +0800

    new_changes

---
+++ basic data structures/parenthesis.py
@@ -0,0 +1,36 @@
+from pythonds.basic.stack import Stack
+
+def parCheck(symbolText):
+
+    s = Stack()
+    balanced = True
+    index = 0
+
+    while index < len(symbolText) and balanced:
+        symbol = symbolText[index]
+
+        if symbol in "[{(":
+            s.push(symbol)
+        else:
+            if s.isEmpty():
+                balanced = False
+            else:
+                top = s.pop()
+                if not matches(top, symbol):
+                    balanced = False
+
+        index += 1
+
+    if balanced and s.isEmpty():
+        return True
+    else:
+        return False
+
+def matches(open, close):
+    opens = "{[("
+    closers = "})]"
+
+    return opens.index(open) == closers.index(close)
+
+print parCheck("(()((((()))()")
+print parCheck("()")
