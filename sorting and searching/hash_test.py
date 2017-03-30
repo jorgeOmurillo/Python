0b8d4b4e8ef07dd8f1a106b169c71764ec2202dc
mnemonicj <j.orlandomurillo@gmail.com>
2017-03-30 17:15:47 +0800

    hashing

--- sorting and searching/hash_test.py
+++ sorting and searching/hash_test.py
@@ -2,14 +2,39 @@ def hashThis(lista,num):
 
     hashed = [None]*num
     res = 0
-    
-    for i in range(len(list)):
+ 
+    for i in lista:
        res = i%num 
-       print res
-       hashed[i] = i%num
-    
+ 
+       if hashed[res] is None:
+           hashed[res] = i
+       else:
+           count = res
+ 
+           while hashed[count] is not None:
+               if count < len(hashed) - 1:
+                   count += 1
+               else:
+                   count = 0
+
+           hashed[count] = i
     print hashed
+    
+    return hashed
 
-listMe = [10,33,4]
+def position(lista, num):
+    
+    found = False
+    count = 0
 
-hashThis(listMe, 5)
+    while not found and count < len(lista)-1:
+        if num == lista[count]:
+            found = True
+        else:
+            count += 1
+    return found
+
+
+listMe = [113 , 117 , 97 , 100 , 114 , 108 , 116 , 105 , 99]
+
+print position(hashThis(listMe, 11), 22)
