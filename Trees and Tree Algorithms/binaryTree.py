92d4ca5c28143a7810736e508d5d827fe7974108
Jorge Murillo <j.orlandomurillo@gmail.com>
2018-04-09 17:37:11 +0800

    binary search tree

--- Trees and Tree Algorithms/binaryTree.py
+++ Trees and Tree Algorithms/binaryTree.py
@@ -1,36 +1,4 @@
 class BinaryTree:
-    def __init__(self, root):
-        self.key = root
-        self.left = None
-        self.right = None
-
-    def insert_left(self, value):
-        if self.left == None:
-            self.left = BinaryTree(value)
-        else:
-            temp = BinaryTree(value)
-            temp.left = self.left
-            self.left = temp
-
-    def insert_right(self, value):
-        if self.right == None:
-            self.right = BinaryTree(value)
-        else:
-            temp = BinaryTree(value)
-            temp.right = self.right
-            self.righ = temp
-
-    def get_right_child(self):
-        return self.right
-
-    def get_left_child(self):
-        return self.left
-
-    def set_root(self, value):
-        self.key = value
-
-    def get_root(self):
-        return self.key
 
 r = BinaryTree('a')
 print(r.get_root())
